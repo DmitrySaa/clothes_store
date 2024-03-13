b2a376d249e09f5590b1a1318cf98c6f33fac701
keyloud <kosolapov10@list.ru>
2024-03-13 17:46:14 +0400

    first_commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./styles/App.css"
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./components/AppRouter";
-import Shop from "./pages/Shop";
+import NavBar from "./components/NavBar";
 
 
 function App() {
 
   return (
+    
     <BrowserRouter>
+      <NavBar />
       <AppRouter />
     </BrowserRouter>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const NavBar = (props) => {
   return (
     <div>
-      
+      <h1>Я ЛОУСКИЛЛ В ДОТЕ</h1>
     </div>
   )
 };
