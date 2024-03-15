732383786e315eea89cc7ecdc2768800e8e8346b
DmitrySaa <samss4598@gmail.com>
2024-03-15 11:28:27 +0400

    Теперь идет проверка авторизации user из Context

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,14 +1,16 @@
-import React from "react"
+import React, { useContext } from "react"
 import { Route, Routes } from 'react-router-dom'
 import { authRoutes, publicRoutes } from "../routes";
+import { Context } from "..";
 
 
 const AppRouter = (props) => {
-    const isAuth = true;
+    const {user} = useContext(Context)
 
+    console.log(user)
     return (
         <Routes>
-            {isAuth && authRoutes.map(({path, Component}) => 
+            {user.isAuth && authRoutes.map(({path, Component}) => 
                 <Route key={path} path={path} element={<Component/>}/>
             )}
 
