31274ca0c434a9e13779bb392e46e93a4ecbec7a
DmitrySaa <samss4598@gmail.com>
2024-03-15 11:27:00 +0400

    Context для user

--- src/store/UserStore.jsx
+++ src/store/UserStore.jsx
@@ -6,4 +6,18 @@ export default class UserStore {
         this._user = {}
         makeAutoObservable(this)
     }
+
+    setIsAuth(bool) {
+        this._isAuth = bool
+    }
+    setUser(user) {
+        this._user = user
+    }
+
+    get isAuth() {
+        return this._isAuth
+    }
+    get user() {
+        return this._user
+    }
 }
