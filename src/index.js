859f37060ca831042ffc4bd97ec1319613104174
DmitrySaa <samss4598@gmail.com>
2024-03-15 11:25:36 +0400

    Добавлен Context для User и Clothes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import UserStore from './store/UserStore';
+import ClothesStore from './store/ClothesStore';
+
+export const Context = createContext(null)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App/>
+    <Context.Provider value={{
+        user: new UserStore(),
+        clothes: new ClothesStore()
+    }}>
+        <App/>
+    </Context.Provider>
 );
 
