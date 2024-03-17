64507620246ba045760994a73335c59459391741
DmitrySaa <samss4598@gmail.com>
2024-03-17 14:57:11 +0400

    создали страничку авторизации/регистрации

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,10 +1,27 @@
 import React from "react"
+import { Card, Container, Form } from "react-bootstrap";
+
 
 const Auth = (props) => {
   return (
-    <div>
-      AUTH
-    </div>
+    <Container 
+    className="d-flex justify-content-center align-items-center"
+    style={{height: window.innerHeight-250}}
+    >
+      <Card style={{width: 600}} className="p-5">
+        <h2 className="m-auto">Авторизация</h2>
+        <Form className="d-fles flex-column">
+            <Form.Control
+              className="mt-3"
+              placeholder="Введите email..."
+            />
+            <Form.Control
+              className="mt-3"
+              placeholder="Введите пароль..."
+            />
+        </Form>
+      </Card>
+    </Container>
   )
 };
 
