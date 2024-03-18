5a2a0cccdb6f90f5fc466745c7814d2c6d5bf5d6
keyloud <kosolapov10@list.ru>
2024-03-19 03:14:22 +0400

    Сменил БД на postrgres. Сделал модели сущностей и подключение к базе данных. Добавил сущности в бд.

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ module.exports = new Sequelize(
     process.env.DB_USER, //Пользователь
     process.env.DB_PASSWORD, // Пароль
     {
-        dialect:'mysql',
+        dialect:'postgres',
         host: process.env.DB_HOST,
         port: process.env.DB_PORT
     }
--- server/server.js
+++ server/server.js
@@ -1,18 +1,30 @@
 require('dotenv').config();
 const express = require('express');
-const  sequelize = require('./db');
+const sequelize = require('./db');
 const PORT = process.env.PORT || 5000;
+const models = require('./models/models')
+const cors = require('cors')
 
 const app = express();
 
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Working!' })
+})
+
+
 //Подключаемся к базе данных
 const start = async () => {
-    try {
-        await sequelize.authenticate();
-        await sequelize.sync();
-        console.log('Соединение с БД было успешно установлено')
-      } catch (e) {
-        console.log('Невозможно выполнить подключение к БД: ', e)
-      }
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    console.log('Соединение с БД было успешно установлено')
+    app.listen(PORT, () => console.log("Server started on port:" + PORT));
+  } catch (e) {
+    console.log('Невозможно выполнить подключение к БД: ', e)
+  }
 }
-app.listen(PORT, () => console.log("Server started on port:" + PORT));
+
+start()
