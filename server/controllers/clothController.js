898f149063c8855f3aa9364705734ebf820cf572
kosol <kosolapov10@list.ru>
2024-05-16 23:53:44 +0400

    CRUD для Controllers

--- server/controllers/clothController.js
+++ server/controllers/clothController.js
@@ -1,12 +1,67 @@
+const uuid = require('uuid')
+const path = require('path')
+const { Clothes, ClothesInfo } = require('../models/models')
+const ApiError = require('../error/ApiError')
+
+
 class ClothController {
-    async create (req,res) {
+    // Создание Бренда
+    async create(req, res, next) {
+        try {
+            let { name, price, brandId, typeId, info } = req.body
+            const { img } = req.files // запрос картинки
+            let fileName = uuid.v4() + ".jpg"
+            img.mv(path.resolve(__dirname, '..', 'static', fileName))
+            const clothes = await Clothes.create({ name, price, brandId, typeId, img: fileName })
 
+            if(info) { 
+                info = JSON.parse(info);
+                info.forEach(element => 
+                    ClothesInfo.create({
+                        title: element.title,
+                        description: element.description,
+                        clothId: clothes.id
+                    })
+                )
+            }
+            
+            return res.json(clothes)
+        } catch (error) {
+            next(ApiError.badRequest(error.message))
+        }
     }
-    async getAll (req, res) {
+    // Запрос всех Брендов
+    async getAll(req, res) {
+        let { brandId, typeId, limit, page } = req.query;
+        page = page || 1;
+        limit = limit || 9;
+        let offset = page * limit - limit
+        let clothes;
+        if (!brandId && !typeId) {
+            clothes = await Clothes.findAndCountAll({ limit, offset });
+        }
+        if (!brandId && typeId) {
+            clothes = await Clothes.findAndCountAll({ where: { typeId }, limit, offset })
+        }
+        if (brandId && !typeId) {
+            clothes = await Clothes.findAndCountAll({ where: { brandId }, limit, offset })
+        }
+        if (brandId && typeId) {
+            clothes = await Clothes.findAndCountAll({ where: { brandId, typeId }, limit, offset })
+        }
 
+        return res.json(clothes); // возвращаем массив одежды
     }
-    async getOne (req, res) {
-    
+    // Запрос одного какого-бренда и его информации.
+    async getOne(req, res) {
+        const {id} = req.params;
+        const clothes = await Clothes.findOne(
+            {
+                where: {id},
+                include: [{model: ClothesInfo, as: 'info'}]
+            },
+        )
+        return res.json(clothes);
     }
 }
 
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,13 +1,18 @@
+const ApiError = require("../error/ApiError")
+
 class UserController {
     async registration (req, res) {
-
+        // Закончил на регистрации и JWT Token
     }
     async login (req, res) {
         
     }
-    async check (req, res) {
+    async check (req, res, next) {
         const {id} = req.query
-        res.json(id) 
+        if(!id){
+            return next(ApiError.badRequest('Не задан ID'))
+        }
+        res.json(id);
     }
 }
 
