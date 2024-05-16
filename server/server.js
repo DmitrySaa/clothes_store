b48c6a22c19afc93e49fde9feec926fa3d25d341
kosol <kosolapov10@list.ru>
2024-05-16 23:52:54 +0400

    Сделал загрузку файлов  в statick

--- server/server.js
+++ server/server.js
@@ -4,13 +4,19 @@ const sequelize = require('./db');
 const PORT = process.env.PORT || 5000;
 const models = require('./models/models');
 const cors = require('cors');
+const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+const path = require('path');
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static'))) // получаем файлы в static
+app.use(fileUpload({}))
 app.use('/api', router)
+app.use(errorHandler) // обязательно вызываем в конце. Обработка ошибок
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Working!' })
